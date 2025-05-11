7257509a450a119e6237ab58a0a611f5ef64eb39
Wicodi <zizking15@gmail.com>
2025-05-11 21:13:07 +0200

    wip: preparet menu

--- untils/Models.py
+++ untils/Models.py
@@ -1,2 +1,7 @@
-def users():
-    return None
+users = [
+    {"name": "Wiktor", "location": "Kutno", "posts": 500},
+    {"name": "Michał", "location": "Krasnystaw", "posts": 400},
+    {"name": "Ksavier", "location": "Grudziądz", "posts": 300},
+    {"name": "Damian", "location": "Kraków", "posts": 200},
+
+]
